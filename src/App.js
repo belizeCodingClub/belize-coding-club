77de332d5835e111761d15222ded6e796983d5bb
Luis Rodriguez <luisrodge@outlook.com>
2021-02-08 17:38:28 -0600

    Minor update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="App-topbar container text-center">
+      <div className="App-topbar  text-center">
         <p>
           Call or{" "}
           <a
