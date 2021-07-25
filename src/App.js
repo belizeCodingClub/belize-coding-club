375f657af3781f94064e3c4c86dca4d8f6b56dc3
Luis Rodriguez <luisrodge@outlook.com>
2021-07-24 20:59:01 -0600

    Add discord widget and discord join buttons

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ function App() {
     <div className="App">
       <div className="App-topbar  text-center">
         <p>
-          <span style={{ paddingRight: 10 }}>Get in touch</span>
-          <a
-            href="https://wa.me/5016082077"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="App-link"
-          >
-            501-608-2077
-          </a>
-          <span style={{ marginLeft: 10, marginRight: 10 }}>|</span>
+          <span>Get in touch with us - </span>
+
           <a href="mailto:hello@belizecodingclub.org" className="App-link">
             hello@belizecodingclub.org
           </a>
@@ -22,16 +14,22 @@ function App() {
       </div>
       <header className="App-header">
         <div className="Header-left">
-          <h1>belizeCodingClub</h1>
+          <h1>
+            {"<"}belizeCodingClub{"/>"}
+          </h1>
           <small>
             <span>Meet</span> <span>Learn</span> <span>Grow</span>
           </small>
         </div>
-        <div className="Header-col">
-          <div className="Meetup-date-box">
-            <h3>First Virtual Meetup</h3>
-            <h3>Coming Soon</h3>
-          </div>
+        <div>
+          <iframe
+            src="https://discord.com/widget?id=865450608157130783&theme=dark"
+            width="100%"
+            height="300"
+            allowtransparency="true"
+            frameborder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+          ></iframe>
         </div>
       </header>
 
@@ -46,12 +44,12 @@ function App() {
           </p>
           <div>
             <a
-              href="https://forms.gle/m4HMPW7URcC4M9Ek7"
+              href="https://discord.gg/RWAc3kcZVq"
               className="btn"
               target="_blank"
               rel="noreferrer"
             >
-              Join club (it's free)
+              Join our Discord
             </a>
           </div>
         </div>
@@ -67,7 +65,6 @@ function App() {
                 Joining our club will provide you with a network of relatable
                 people just like yourself. You'll meet and interact with people
                 not only from your district but from across the entire country.
-                How? With our virtual meetups and chat groups.
               </p>
             </div>
           </div>
@@ -79,38 +76,39 @@ function App() {
               <p>
                 The ultimate goal of this club is to bring people together who
                 are passionate about coding regardless of their career stage or
-                current level of knowledge. Our virtual meetups are meant to
-                introduce people to one another, talk about different
-                technologies, side projects, share ideas, and much more. We also
-                plan on having guest speakers from local and foreign tech
-                companies share their experience and provide practical advice
-                about coding and all things tech.
+                current level of knowledge. This club aims to bring the
+                developer community here in Belize closer and in doing so
+                provide a platform where people can talk about different
+                technologies, share side projects, share ideas, and much much
+                more. We also plan on having meetups as soon as it's safe to do
+                so.
               </p>
             </div>
           </div>
         </div>
       </div>
+
       <div className="join-cta text-center">
         <div className="container">
           <h1 style={{ margin: 0 }}>Join us today</h1>
           <p style={{ margin: 0 }}>
-            You won't want to miss out on our first virtual meetup so be sure to
-            click the button below and become a club member today.
+            You won't want to miss out so be sure to click the button below and
+            become a member of the community today.
           </p>
           <a
-            href="https://forms.gle/m4HMPW7URcC4M9Ek7"
+            href="https://discord.gg/RWAc3kcZVq"
             className="btn"
             target="_blank"
             rel="noreferrer"
             style={{ marginTop: "34px" }}
           >
-            Join club (it's free)
+            Join our Discord
           </a>
         </div>
       </div>
       <footer className="text-center">
         <div className="container">
-          <p>Copyright © 2021 belizeCodingClub - Powered by you</p>
+          <p>belizeCodingClub - hello@belizecodingclub.org</p>
         </div>
       </footer>
     </div>
